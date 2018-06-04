9265dc94f201c5aa8258f72ac38a446e2dbeb5c1
Robert Kraig <robertkraig@gmail.com>
2018-06-04 16:35:55 -0700

    Adding laravel cashier and information on how to setup laradock to development.md

--- Development.md
+++ Development.md
@@ -6,3 +6,22 @@ This will simplify installing dependencies and managing configuration. First ins
 simply run `docker-compose up`. The app should now be running on on port 8000.
 
 To shut down the server use `docker-compose down`
+
+
+Development
+==============
+initial build:
+- npm install
+- composer install
+
+- composer dev-update `updates IDE tooling autocompletes`
+
+Strip Debugging
+=============
+- https://stripe.com/docs/testing `Has a list of test CreditCard Numbers`
+
+Development Environment w/ Laradock
+==============
+- Install Laradock `git clone https://github.com/Laradock/laradock.git`
+- Run Laradock Containers `cd laradock && docker-compose up -d nginx mysql phpmyadmin workspace`
+- Go inside of workspace Container `cd laradock && docker-compose exec workspace bash`
--- app/Http/Controllers/Stripe/StripeController.php
+++ app/Http/Controllers/Stripe/StripeController.php
@@ -4,47 +4,48 @@ namespace App\Http\Controllers\Stripe;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Stripe\Invoice;
+use \Stripe\Stripe;
+use \Stripe\Customer;
+use \Stripe\Charge;
 
 class StripeController extends Controller
 {
-    //
+  //
 
-    public function config() {
-      $stripe = array(
-        "secret_key"      => "sk_test_BQokikJOvBiI2HlWgH4olfQ2",
-        "publishable_key" => "pk_test_6pRNASCoBOKtIshFeQd4XMUh"
-      );
+  public function config()
+  {
+    $SECRET_KEY = config('services.stripe.secret');
+    $PUBLISHABLE_KEY = config('services.stripe.key');
 
-      \Stripe\Stripe::setApiKey($stripe['secret_key']);
+    Stripe::setApiKey($SECRET_KEY);
 
-      return view('donate', ['stripe_key' =>  $stripe['publishable_key'], 'result' => 0] );
-    }
+    return view('donate', ['stripe_key' => $PUBLISHABLE_KEY, 'result' => 0]);
+  }
 
-    public function charge(Request $request) {
-      $token  = $request->get('stripeToken');
-      $amount = $request->get('amount-us')*100;
-      $email = $request->get('stripeEmail');
+  public function charge(Request $request)
+  {
+    $token = $request->get('stripeToken');
+    $amount = $request->get('amount-us') * 100;
+    $email = $request->get('stripeEmail');
 
-      $stripe = array(
-        "secret_key"      => "sk_test_BQokikJOvBiI2HlWgH4olfQ2",
-        "publishable_key" => "pk_test_6pRNASCoBOKtIshFeQd4XMUh"
-      );
+    $SECRET_KEY = config('services.stripe.secret_key');
 
-      \Stripe\Stripe::setApiKey($stripe['secret_key']);
+    Stripe::setApiKey($SECRET_KEY);
 
-      $customer = \Stripe\Customer::create(array(
-          'email' => $email,
-          'source'  => $token
-      ));
+    $customer = Customer::create(array(
+      'email' => $email,
+      'source' => $token
+    ));
 
-      $charge = \Stripe\Charge::create(array(
-          'customer' => $customer->id,
-          'amount'   => $amount,
-          'currency' => 'usd'
-      ));
+    $charge = Charge::create(array(
+      'customer' => $customer->id,
+      'amount' => $amount,
+      'currency' => 'usd'
+    ));
 
-      //return view('donate', ['result' =>  'Successfully charged $50.00!'] );
-      return view('confirm');
-    }
+    //return view('donate', ['result' =>  'Successfully charged $50.00!'] );
+    return view('confirm');
+  }
 
 }
